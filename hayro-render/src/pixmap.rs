228f54857772f6c14248187b505aefe13a52f845
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-07-18 22:59:36 +0200

    Make Xref cloneable

--- hayro-render/src/pixmap.rs
+++ hayro-render/src/pixmap.rs
@@ -10,7 +10,7 @@ use std::vec;
 use std::vec::Vec;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Pod, Zeroable)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(carfeature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[repr(C)]
 pub struct PremulRgba8 {
     /// Red component.
