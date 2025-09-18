64bafb61d0884f43ab49c536451263498b4690b7
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-09-18 23:30:32 +0200

    Make parsing of trailer dictionaries in invalid PDFs more robust

--- hayro-syntax/src/xref.rs
+++ hayro-syntax/src/xref.rs
@@ -2,7 +2,7 @@
 
 use crate::crypto::{DecryptionError, DecryptionTarget, Decryptor, get};
 use crate::data::Data;
-use crate::object::Array;
+use crate::object::{Array, MaybeRef};
 use crate::object::Dict;
 use crate::object::Name;
 use crate::object::ObjectIdentifier;
@@ -58,7 +58,7 @@ pub(crate) fn fallback(data: PdfData) -> Option<XRef> {
 
 fn fallback_xref_map(data: &[u8]) -> (XrefMap, Option<&[u8]>) {
     let mut xref_map = FxHashMap::default();
-    let mut trailer_dict = None;
+    let mut trailer_dicts = vec![];
 
     let mut r = Reader::new(data);
 
@@ -76,7 +76,7 @@ fn fallback_xref_map(data: &[u8]) -> (XrefMap, Option<&[u8]>) {
             dummy_ctx.obj_number = Some(obj_id);
         } else if let Some(dict) = r.read::<Dict>(dummy_ctx) {
             if dict.contains_key(SIZE) && dict.contains_key(ROOT) {
-                trailer_dict = Some(dict.clone());
+                trailer_dicts.push(dict);
             }
 
             if let Some(stream) = old_r.read::<Stream>(dummy_ctx)
@@ -101,6 +101,37 @@ fn fallback_xref_map(data: &[u8]) -> (XrefMap, Option<&[u8]>) {
             break;
         }
     }
+    
+    // Try to choose the right trailer dict by doing basic validation.
+    let mut trailer_dict = None;
+    
+    for dict in trailer_dicts {
+        if let Some(root_id) = dict.get_raw::<Dict>(ROOT) {
+            let check = |dict: &Dict| -> bool {
+                dict.contains_key(PAGES)
+            };
+            
+            match root_id {
+                MaybeRef::Ref(r) => {
+                    if let Some(EntryType::Normal(offset)) = xref_map.get(&r.into()) {
+                        let mut reader = Reader::new(&data[*offset..]);
+                        
+                        if let Some(obj) =reader.read_with_context::<IndirectObject<Dict>>(dummy_ctx)
+                        && check(&obj.clone().get()) {
+                            trailer_dict = Some(dict);
+                        }
+                    }
+                }
+                MaybeRef::NotRef(d) => {
+                    if check(&d) {
+                        trailer_dict = Some(dict);
+                    }
+                }
+            }
+        }
+    }
+    
+    eprintln!("{:?}", trailer_dict);
 
     (xref_map, trailer_dict.map(|d| d.data()))
 }
