2027c3d219972ca4d6f9ec2f86c744ad73efce95
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-09-19 13:37:26 +0200

    Add a working test case

--- hayro-tests/tests/render.rs
+++ hayro-tests/tests/render.rs
@@ -1008,6 +1008,7 @@ use crate::run_render_test;
 #[test] fn pdfbox_3977() { run_render_test("pdfbox_3977", "downloads/pdfbox/3977.pdf", None); }
 #[test] fn pdfbox_3979() { run_render_test("pdfbox_3979", "downloads/pdfbox/3979.pdf", None); }
 #[test] fn pdfbox_3990() { run_render_test("pdfbox_3990", "downloads/pdfbox/3990.pdf", None); }
+#[test] fn pdfbox_4006() { run_render_test("pdfbox_4006", "downloads/pdfbox/4006.pdf", None); }
 #[test] fn pdfbox_4010() { run_render_test("pdfbox_4010", "downloads/pdfbox/4010.pdf", None); }
 #[test] fn pdfbox_4015() { run_render_test("pdfbox_4015", "downloads/pdfbox/4015.pdf", None); }
 #[test] fn pdfbox_4059() { run_render_test("pdfbox_4059", "downloads/pdfbox/4059.pdf", None); }
