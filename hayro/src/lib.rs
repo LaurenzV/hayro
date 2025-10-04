2f36a93fc55d8c7c892a366dbee27b11eb90ecb8
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-10-04 22:38:35 +0200

    Finalize

--- hayro/src/lib.rs
+++ hayro/src/lib.rs
@@ -35,7 +35,7 @@ This crate has two optional features:
 #![deny(missing_docs)]
 
 use crate::renderer::Renderer;
-use hayro_interpret::Context;
+use hayro_interpret::{BlendMode, Context};
 use hayro_interpret::Device;
 use hayro_interpret::FillRule;
 pub use hayro_interpret::font::{FontData, FontQuery, StandardFont};
@@ -118,7 +118,7 @@ pub fn render(
         fill: FillRule::NonZero,
     });
 
-    device.push_transparency_group(1.0, None);
+    device.push_transparency_group(1.0, None, BlendMode::Normal);
     interpret_page(page, &mut state, &mut device);
     device.pop_transparency_group();
 
