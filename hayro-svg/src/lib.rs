2b96f4d4ffec099e455045d966ba62a714b07d23
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-10-04 23:40:19 +0200

    Add test case for SVG

--- hayro-svg/src/lib.rs
+++ hayro-svg/src/lib.rs
@@ -96,7 +96,12 @@ impl<'a> SvgRenderer<'a> {
         }
     }
 
-    fn push_transparency_group_inner(&mut self, opacity: f32, mask: Option<MaskKind<'a>>) {
+    fn push_transparency_group_inner(
+        &mut self,
+        opacity: f32,
+        mask: Option<MaskKind<'a>>,
+        blend_mode: BlendMode,
+    ) {
         let mask_id = mask.map(|m| self.get_mask_id(m));
 
         self.xml.start_element("g");
@@ -104,6 +109,30 @@ impl<'a> SvgRenderer<'a> {
         if let Some(mask_id) = mask_id {
             self.xml
                 .write_attribute_fmt("mask", format_args!("url(#{mask_id})"));
+        }
+
+        if blend_mode != BlendMode::Normal {
+            let bm_name = match blend_mode {
+                BlendMode::Normal => "normal",
+                BlendMode::Multiply => "multiply",
+                BlendMode::Screen => "screen",
+                BlendMode::Overlay => "overlay",
+                BlendMode::Darken => "darken",
+                BlendMode::Lighten => "lighten",
+                BlendMode::ColorDodge => "color-dodge",
+                BlendMode::ColorBurn => "color-burn",
+                BlendMode::HardLight => "hard-light",
+                BlendMode::SoftLight => "soft-light",
+                BlendMode::Difference => "difference",
+                BlendMode::Exclusion => "exclusion",
+                BlendMode::Hue => "hue",
+                BlendMode::Saturation => "saturation",
+                BlendMode::Color => "color",
+                BlendMode::Luminosity => "luminosity",
+            };
+
+            self.xml
+                .write_attribute("style", &format!("mix-blend-mode:{}", bm_name));
         }
 
         if !opacity.is_nearly_equal(1.0) {
@@ -151,6 +180,20 @@ impl<'a> SvgRenderer<'a> {
             );
         }
     }
+
+    fn with_group(&mut self, func: impl FnOnce(&mut SvgRenderer<'a>)) {
+        let push_group = self.cur_mask.is_some() || self.cur_blend_mode != BlendMode::Normal;
+
+        if push_group {
+            self.push_transparency_group(1.0, self.cur_mask.clone(), self.cur_blend_mode);
+        }
+
+        func(self);
+
+        if push_group {
+            self.pop_transparency_group();
+        }
+    }
 }
 
 impl<'a> Device<'a> for SvgRenderer<'a> {
@@ -169,17 +212,9 @@ impl<'a> Device<'a> for SvgRenderer<'a> {
         paint: &Paint<'a>,
         draw_mode: &PathDrawMode,
     ) {
-        let push_group = self.cur_mask.is_some();
-
-        if push_group {
-            self.push_transparency_group(1.0, self.cur_mask.clone(), self.cur_blend_mode);
-        }
-
-        Self::draw_path(self, path, transform, paint, draw_mode);
-
-        if push_group {
-            self.pop_transparency_group();
-        }
+        self.with_group(|r| {
+            Self::draw_path(r, path, transform, paint, draw_mode);
+        })
     }
 
     fn push_clip_path(&mut self, clip_path: &ClipPath) {
@@ -201,7 +236,7 @@ impl<'a> Device<'a> for SvgRenderer<'a> {
         mask: Option<SoftMask<'a>>,
         blend_mode: BlendMode,
     ) {
-        self.push_transparency_group_inner(opacity, mask.map(MaskKind::SoftMask));
+        self.push_transparency_group_inner(opacity, mask.map(MaskKind::SoftMask), blend_mode);
     }
 
     fn draw_glyph(
@@ -212,20 +247,13 @@ impl<'a> Device<'a> for SvgRenderer<'a> {
         paint: &Paint<'a>,
         draw_mode: &GlyphDrawMode,
     ) {
-        let push_group = self.cur_mask.is_some();
-
-        if push_group {
-            self.push_transparency_group(1.0, self.cur_mask.clone(), self.cur_blend_mode);
-        }
-
-        Self::draw_glyph(self, glyph, transform, glyph_transform, paint, draw_mode);
-
-        if push_group {
-            self.pop_transparency_group();
-        }
+        self.with_group(|r| {
+            Self::draw_glyph(r, glyph, transform, glyph_transform, paint, draw_mode);
+        })
     }
 
     fn draw_image(&mut self, image: Image<'a, '_>, transform: Affine) {
+        // TODO: With Self::Group?
         match image {
             Image::Stencil(s) => {
                 s.with_stencil(|s, paint| {
