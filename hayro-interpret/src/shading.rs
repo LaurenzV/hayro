104896847b570c430a0540b250acd5f184e5903a
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-10-17 22:45:28 +0200

    Inflate the triangles of shadings

--- hayro-interpret/src/shading.rs
+++ hayro-interpret/src/shading.rs
@@ -748,9 +748,25 @@ where
                 point: p11,
                 colors: interpolate(Point::new(u1, v1)),
             };
+            
+            let inflate_point = |p: Point, mid: Point| -> Point {
+                const INFLATION_FACTOR: f64 = 1.025;
+                mid + (p - mid) * INFLATION_FACTOR
+            };
 
-            buffer.push(Triangle::new(v00.clone(), v10.clone(), v01.clone()));
-            buffer.push(Triangle::new(v10.clone(), v11.clone(), v01.clone()));
+            // Converting to triangles is lossy and can lead to gaps. To make this a bit better,
+            // we slightly inflate the triangles.
+            let inflate = |mut triangle: Triangle| {
+                let mid = triangle.kurbo_tri.centroid();
+                triangle.p0.point = inflate_point(triangle.p0.point, mid);
+                triangle.p1.point = inflate_point(triangle.p1.point, mid);
+                triangle.p2.point = inflate_point(triangle.p2.point, mid);
+                
+                triangle
+            };
+            
+            buffer.push(inflate(Triangle::new(v00.clone(), v10.clone(), v01.clone())));
+            buffer.push(inflate(Triangle::new(v10.clone(), v11.clone(), v01.clone())));
         }
     }
 }
