40ad89963d1f2e97a6babaa4898ec5bfacc29a32
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-12-02 18:01:09 +0100

    Clippy

--- hayro-jpeg2000/src/build.rs
+++ hayro-jpeg2000/src/build.rs
@@ -63,7 +63,7 @@ fn build_decompositions(
                 );
 
                 let precincts =
-                    build_precincts(&resolution_tile, sub_band_rect, tile_ctx, storage)?;
+                    build_precincts(resolution_tile, sub_band_rect, tile_ctx, storage)?;
 
                 let added_coefficients = (sub_band_rect.width() * sub_band_rect.height()) as usize;
                 let coefficients = coefficient_counter..(coefficient_counter + added_coefficients);
