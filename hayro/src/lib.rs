50950998e58495e18e172509d6c4d9b708cd7692
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-12-05 19:28:41 +0100

    Re-export everything of vello-cpu in hayro

--- hayro/src/lib.rs
+++ hayro/src/lib.rs
@@ -46,7 +46,8 @@ use hayro_interpret::{ClipPath, interpret_page};
 pub use hayro_interpret::{InterpreterSettings, Pdf};
 use kurbo::{Affine, Rect, Shape};
 use std::ops::RangeInclusive;
-pub use vello_cpu::Pixmap;
+pub use vello_cpu;
+
 use vello_cpu::color::AlphaColor;
 use vello_cpu::color::Srgb;
 use vello_cpu::color::palette::css::TRANSPARENT;
