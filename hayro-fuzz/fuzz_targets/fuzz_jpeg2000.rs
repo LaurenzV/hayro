1dc8a3a52c64163433530c772199f1581773ce47
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-12-24 09:30:48 +0100

    Update

--- hayro-fuzz/fuzz_targets/fuzz_jpeg2000.rs
+++ hayro-fuzz/fuzz_targets/fuzz_jpeg2000.rs
@@ -6,7 +6,7 @@ use image::ImageDecoder;
 
 fuzz_target!(|data: &[u8]| {
     let settings = DecodeSettings::default();
-    if let Ok(image) = Image::new(&data, &settings) {
+    if let Ok(image) = Image::new(data, &settings) {
         let mut buf = vec![0_u8; image.total_bytes() as usize];
         let _ = image.read_image(&mut buf);
     }
