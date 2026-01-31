4b18448b0bf3475bb0e90916c8e0d1efef05b2fd
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2026-01-31 09:48:12 +0100

    clippy

--- hayro-jbig2/examples/parse.rs
+++ hayro-jbig2/examples/parse.rs
@@ -4,7 +4,7 @@ fn main() {
     // let base_path = concat!(env!("CARGO_MANIFEST_DIR"), "/test-inputs/serenity/");
     // let filename = "bitmap-symbol-refine.jbig2";
 
-    let path = format!("/Users/lstampfl/Programming/hayro/test.jb2");
+    let path = "/Users/lstampfl/Programming/hayro/test.jb2";
     let data = std::fs::read(&path).expect("Failed to read test file");
 
     // println!("Decoding: {filename} ({} bytes)", data.len());
