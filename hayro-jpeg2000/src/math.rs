e364e59ae143c29a2a481612368f819a4420ca0f
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-02-14 00:59:53 +0000

    use the dedicated store method without roundtripping through a possible memcpy

--- hayro-jpeg2000/src/math.rs
+++ hayro-jpeg2000/src/math.rs
@@ -49,7 +49,7 @@ mod inner {
 
         #[inline(always)]
         pub(crate) fn store(self, slice: &mut [f32]) {
-            slice[..SIMD_WIDTH].copy_from_slice(self.inner.as_slice());
+            self.inner.store_slice(&mut slice[..SIMD_WIDTH]);
         }
 
         #[inline(always)]
