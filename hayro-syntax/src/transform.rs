904fa9202a7fa0d003a3bb857763c08992bc93b6
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2026-03-08 10:22:20 +0100

    visibilities

--- hayro-syntax/src/transform.rs
+++ hayro-syntax/src/transform.rs
@@ -1,7 +1,7 @@
 // Copyright 2018 the Kurbo Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Affine transforms.
+//! 2D affine transformations.
 
 use core::ops::Mul;
 
@@ -13,41 +13,20 @@ impl Affine {
     /// The identity transform.
     pub const IDENTITY: Affine = Affine::scale(1.0);
 
-    /// Construct an affine transform from coefficients.
-    ///
-    /// If the coefficients are `(a, b, c, d, e, f)`, then the resulting
-    /// transformation represents this augmented matrix:
-    ///
-    /// ```text
-    /// | a c e |
-    /// | b d f |
-    /// | 0 0 1 |
-    /// ```
-    ///
-    /// Note that this convention is transposed from PostScript and
-    /// Direct2D, but is consistent with the
-    /// [Wikipedia](https://en.wikipedia.org/wiki/Affine_transformation)
-    /// formulation of affine transformation as augmented matrix. The
-    /// idea is that `(A * B) * v == A * (B * v)`, where `*` is the
-    /// [`Mul`] trait.
     #[inline(always)]
-    pub const fn new(c: [f64; 6]) -> Affine {
+    pub(crate) const fn new(c: [f64; 6]) -> Affine {
         Affine(c)
     }
 
-    /// An affine transform representing uniform scaling.
     #[inline(always)]
     pub(crate) const fn scale(s: f64) -> Affine {
         Affine([s, 0.0, 0.0, s, 0.0, 0.0])
     }
 
-    /// A clockwise 90° rotation (in a Y-down coordinate system).
     pub(crate) const ROTATE_CW_90: Affine = Affine::new([0.0, 1.0, -1.0, 0.0, 0.0, 0.0]);
 
-    /// A counter-clockwise 90° rotation (in a Y-down coordinate system).
     pub(crate) const ROTATE_CCW_90: Affine = Affine::new([0.0, -1.0, 1.0, 0.0, 0.0, 0.0]);
 
-    /// An affine transform representing translation.
     #[inline(always)]
     pub(crate) const fn translate(p: (f64, f64)) -> Affine {
         Affine([1.0, 0.0, 0.0, 1.0, p.0, p.1])
